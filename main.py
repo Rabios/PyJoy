e7ebae5ffa8927509adf3bd365268e67dbf74cbe
Rabia Alhaffar <steria773@gmail.com>
2020-07-19 11:19:15 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+# Written by Rabia Alhaffar in 18/July/2020
+# Main info
+pyjoy_version = "v0.0.1"   # Version
+author = "Rabia Alhaffar"  # Author
+use_cli = False            # CLI is disabled by default for distributing in games
