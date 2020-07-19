fa59eea661a9e8b31f2e5a1fc3c8482b8b2e17eb
Rabia Alhaffar <steria773@gmail.com>
2020-07-20 00:20:37 +0300

    Update example_01.py

--- examples/example_01.py
+++ examples/example_01.py
@@ -5,18 +5,29 @@
 
 from pyjoy import *
 
+# Create window
 w = window(800, 600, "MYGAME", False)
+
+# Create rectangle class
 r = rect(0.0, 0.0, 100 / 1000, 100 / 1000)
 while not w.should_close():
+    # Clear in black
     clear(None)
+    
+    # Draw
     draw_rect("fill", r, color(0, 0, 255, 255))
-    if key_down(w, glfw.KEY_W):
+
+    # If key down
+    if key_down(w, glfw.KEY_W) or key_down(w, glfw.KEY_UP):
         r.y += 1 / 1000
-    if key_down(w, glfw.KEY_S):
+    if key_down(w, glfw.KEY_S) or key_down(w, glfw.KEY_DOWN):
         r.y -= 1 / 1000
-    if key_down(w, glfw.KEY_A):
+    if key_down(w, glfw.KEY_A) or key_down(w, glfw.KEY_LEFT):
         r.x -= 1 / 1000
-    if key_down(w, glfw.KEY_D):
+    if key_down(w, glfw.KEY_D) or key_down(w, glfw.KEY_RIGHT):
         r.x += 1 / 1000
+
+    # Render and update game window
     w.update()
     
+close_window()
