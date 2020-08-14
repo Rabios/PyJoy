08d7ca992b03d49f8f0d598c9f1d351595420f79
Rabia Alhaffar <steria773@gmail.com>
2020-08-14 15:39:46 +0300

    Updated to version v0.0.3

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 # Written by Rabia Alhaffar in 18/July/2020
 # Main info
-pyjoy_version = "v0.0.2"         # Version
+pyjoy_version = "v0.0.3"         # Version
 pyjoy_author = "Rabia Alhaffar"  # Author
 pyjoy_use_cli = False            # CLI is disabled by default for distributing in games
 
